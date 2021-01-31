4415a1e77c71733d83fb62688a22e740a54215d2
j-maestre <xemaiestacio@gmail.com>
2021-01-31 11:32:02 +0100

    informacion del commit

--- .github/actions/send_message/index.js
+++ .github/actions/send_message/index.js
@@ -2,9 +2,9 @@ const core = require("@actions/core");
 const TelegramBot = require("node-telegram-bot-api");
 const github = require("@actions/github");
 
-const token = core.getInput("token_bot"); //Token que le pasamos en el flow.yml como id del bot
+const token = core.getInput("token_father");
 const bot = new TelegramBot(token, { polling: false });
-var chat_Id = core.getInput("chatId"); //Token del grupo que le pasamos
+var chat_Id = core.getInput("chatId");
 const name = core.getInput("name");
 const commit = github.context.payload;
 console.log("COOOMIIITTT");
@@ -13,7 +13,7 @@ console.log(commit);
 try {
   bot.sendMessage(
     chat_Id,
-    `Workflow ejecutado correctamente tras el último commit. Saludos ,${name}` //:\n\nCreadoBy:${commit.head_commit.author.name}\Avatar: ${commit.sender.avatar_url}\Url: ${commit.head_commit.url}\Mensage: ${commit.head_commit.message}`
+    `Workflow ejecutado correctamente tras el último commit. Saludos ,${name}:\n\nCreadoBy:${commit.head_commit.author.name}\Avatar: ${commit.sender.avatar_url}\Url: ${commit.head_commit.url}\Mensage: ${commit.head_commit.message}`
   );
 } catch (error) {
   core.setFailed(error.message);
