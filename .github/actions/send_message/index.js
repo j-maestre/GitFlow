86efffbc2e363cc3e268602c5abcf7ba1450c80a
j-maestre <xemaiestacio@gmail.com>
2021-01-31 11:25:13 +0100

    funciona

--- .github/actions/send_message/index.js
+++ .github/actions/send_message/index.js
@@ -1,6 +1,6 @@
 const core = require("@actions/core");
 const TelegramBot = require("node-telegram-bot-api");
-const github = require("@actions/github");
+// const github = require("@actions/github");
 
 const token = core.getInput("token_bot"); //Token que le pasamos en el flow.yml como id del bot
 const bot = new TelegramBot(token, { polling: false });
@@ -13,7 +13,7 @@ console.log(commit);
 try {
   bot.sendMessage(
     chat_Id,
-    `Workflow ejecutado correctamente tras el último commit. Saludos ,${name}:\n\nCreadoBy:${commit.head_commit.author.name}\Avatar: ${commit.sender.avatar_url}\Url: ${commit.head_commit.url}\Mensage: ${commit.head_commit.message}`
+    `Workflow ejecutado correctamente tras el último commit. Saludos ,${name}` //:\n\nCreadoBy:${commit.head_commit.author.name}\Avatar: ${commit.sender.avatar_url}\Url: ${commit.head_commit.url}\Mensage: ${commit.head_commit.message}`
   );
 } catch (error) {
   core.setFailed(error.message);
